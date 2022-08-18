48ab8f9dfd835e9adaa20ebd41345b4765baf61c
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-08-18 14:43:03 +0300

    commit tree 89f8a3bf550b75388691769d3034298edf421126

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+require "pry"
+
+class Dog
+    attr_accessor :name, :owner
+    def initialize(name)
+      @name = name
+    end
+  end
+
+  fido.owner = "Sophie"
+binding.pry
